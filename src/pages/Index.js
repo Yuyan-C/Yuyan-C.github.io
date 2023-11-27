05e56d7714eeb1eacaace9bf69ea00c547dc2ef4
Yuyan-C <chenyuyan@Yuyans-MacBook-Air.local>
2023-11-27 10:57:03 -0500

    update contact&main

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={'Lorem ipsum dolor sit amet'
-      + 'VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni.'}
+    description={''
+      + '.'}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
           <h2><Link to="/">About this site</Link></h2>
           <p>
-            I created my website based on this <a href="https://github.com/mldangelo/personal-site">template</a>.
+            This website is adpated from <a href="https://github.com/mldangelo/personal-site">here</a>.
           </p>
         </div>
       </header>
-      <p>My profile picture is  <a href="https://www.parcjeandrapeau.com/en/biosphere-environment-museum-montreal/">Biosphere</a>, an environment museum and a geodesic dome in Montreal, QC. </p>
-
+      <p> </p>
     </article>
   </Main>
 );
